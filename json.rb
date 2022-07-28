cb37226d34af874173f6a6b935e97518faedfaf5
leviongit <levi.duncan.self@gmail.com>
2022-07-28 19:53:02 +0200

    Fix wording...

--- json.rb
+++ json.rb
@@ -2,105 +2,105 @@ module JSON
   class JSONKeyError < StandardError; end
 
   class << self
-    def write(stream, value, indent_size = 4)
+    def write(str, value, indent_size = 4)
       raise ArgumentError, "Top-level value must be either an Array or a Hash" unless value.is_a?(Array) || value.is_a?(Hash)
 
-      write_value(stream, value, 0, indent_size)
+      write_value(str, value, 0, indent_size)
     end
 
     private
 
-    def write_hash_inline(stream, value, indent_depth, indent_size)
+    def write_hash_inline(str, value, indent_depth, indent_size)
       raise ArgumentError, "value must be a Hash" unless value.is_a?(Hash)
       raise JSONKeyError, "Not all keys in hash are strings/symbols" unless value.keys.all? {
         _1.is_a?(String) || _1.is_a?(Symbol)
       }
 
-      return stream << "{}" if value.keys.length == 0
+      return str << "{}" if value.keys.length == 0
 
-      stream << "{"
+      str << "{"
 
       value.each.with_index { |(k, v), i|
-        stream << "," if i != 0
-        stream << " #{k.to_s.inspect}: "
-        write_value(stream, v, indent_depth, indent_size)
+        str << "," if i != 0
+        str << " #{k.to_s.inspect}: "
+        write_value(str, v, indent_depth, indent_size)
       }
 
-      stream << " }"
+      str << " }"
     end
 
-    def write_hash_block(stream, value, indent_depth, indent_size)
+    def write_hash_block(str, value, indent_depth, indent_size)
       raise ArgumentError, "value must be a Hash" unless value.is_a?(Hash)
       raise JSONKeyError, "Not all keys in hash are strings/symbols" unless value.keys.all? {
         _1.is_a?(String) || _1.is_a?(Symbol)
       }
 
-      stream << "{"
+      str << "{"
 
       value.each.with_index { |(k, v), i|
-        stream << "," if i != 0
-        stream << "\n#{" " * (indent_depth * indent_size)}#{k.to_s.inspect}: "
-        write_value(stream, v, indent_depth, indent_size)
+        str << "," if i != 0
+        str << "\n#{" " * (indent_depth * indent_size)}#{k.to_s.inspect}: "
+        write_value(str, v, indent_depth, indent_size)
       }
 
-      stream << "\n" + (" " * ((indent_depth - 1) * indent_size)) + "}"
+      str << "\n" + (" " * ((indent_depth - 1) * indent_size)) + "}"
     end
 
-    def write_array_inline(stream, value, indent_depth, indent_size)
+    def write_array_inline(str, value, indent_depth, indent_size)
       raise ArgumentError, "value must be an Array" unless value.is_a?(Array)
 
-      return stream << "[]" if value.length == 0
+      return str << "[]" if value.length == 0
 
-      stream << "[ "
+      str << "[ "
 
       value.each.with_index { |v, i|
-        stream << ", " if i != 0
-        write_value(stream, v, indent_depth, indent_size)
+        str << ", " if i != 0
+        write_value(str, v, indent_depth, indent_size)
       }
 
-      stream << " ]"
+      str << " ]"
     end
 
-    def write_array_block(stream, value, indent_depth, indent_size)
+    def write_array_block(str, value, indent_depth, indent_size)
       raise ArgumentError, "value must be an Array" unless value.is_a?(Array)
 
-      stream << "[\n"
+      str << "[\n"
 
       value.each.with_index { |v, i|
-        stream << ",\n" if i != 0
-        stream << " " * (indent_size * indent_depth)
-        write_value(stream, v, indent_depth, indent_size)
+        str << ",\n" if i != 0
+        str << " " * (indent_size * indent_depth)
+        write_value(str, v, indent_depth, indent_size)
       }
 
-      stream << "\n" + (" " * ((indent_depth - 1) * indent_size)) + "]"
+      str << "\n" + (" " * ((indent_depth - 1) * indent_size)) + "]"
     end
 
-    def write_value(stream, value, indent_depth = 0, indent_size = 4)
+    def write_value(str, value, indent_depth = 0, indent_size = 4)
       case value
       when Hash
         if value.keys.length > 1 || value.any? { |_, v| v.is_a?(Array) || v.is_a?(Hash) }
-          write_hash_block(stream, value, indent_depth + 1, indent_size)
+          write_hash_block(str, value, indent_depth + 1, indent_size)
         else
-          write_hash_inline(stream, value, indent_depth + 1, indent_size)
+          write_hash_inline(str, value, indent_depth + 1, indent_size)
         end
       when Array
         if value.length > 1 || value.any? { |_, v| v.is_a?(Array) || v.is_a?(Hash) }
-          write_array_block(stream, value, indent_depth + 1, indent_size)
+          write_array_block(str, value, indent_depth + 1, indent_size)
         else
-          write_array_inline(stream, value, indent_depth + 1, indent_size)
+          write_array_inline(str, value, indent_depth + 1, indent_size)
         end
       when Float
-        stream << value.to_s
+        str << value.to_s
       when String
-        stream << value.inspect
+        str << value.inspect
       when Integer
-        stream << value.to_s
+        str << value.to_s
       when nil
-        stream << "null"
+        str << "null"
       when true
-        stream << "true"
+        str << "true"
       when false
-        stream << "false"
+        str << "false"
       end
     end
   end
