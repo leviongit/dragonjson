21a54f0eb6ac4963f18e617f938827428f21a06c
Pieter van de Bruggen <pvande@gmail.com>
2024-05-19 19:58:18 -0700

    Implement a basic benchmarking script
    This can be run with:
    
    `./dragonruby test --eval test/benchmark.rb --no-tick`
    
    … once a DR executable and font file have been copied into place.

---
+++ test/benchmark.rb
@@ -0,0 +1,32 @@
+require "json"
+
+json = $gtk.read_file("crimes.json")
+
+def benchmark
+  GC.start
+  start = Time.now
+  yield
+  (Time.now - start) * 1000
+end
+
+puts_immediate "Parsing `test/crimes.json` 20 times…"
+
+threshhold = 20.map do
+  i = 0
+  n = json.size
+
+  benchmark do
+    while i < n
+      json.getbyte(i)
+      i += 1
+    end
+  end
+end
+puts_immediate "Threshhold Time: #{threshhold.min.to_sf}ms"
+
+data = 20.map { benchmark { LevisLibs::JSON.parse(json) } }
+mean = data.sum / data.size
+var = data.map { |time| (time - mean) ** 2 }.sum / data.size
+puts_immediate "Average Time: #{mean.to_sf}ms ± #{Math.sqrt(var).to_sf}ms"
+puts_immediate "Fastest Time: #{data.min.to_sf}ms"
+puts_immediate "Slowest Time: #{data.max.to_sf}ms"
